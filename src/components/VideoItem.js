68d7d478d257df7a9d6d1c28e2f56f00af00a028
ramikh96 <ramikh.developer@gmail.com>
2019-12-02 22:44:57 +0200

    Adding some style to the VideoList and VideoItem components

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import VideoList from './VideoList';
+import '../css/VideoItem.css';
 const VideoItem = ({ video }) => {
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} />
-            {video.snippet.title}
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
     );
 };
